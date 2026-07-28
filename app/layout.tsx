2b05edb6d71e8faf261d7c058fa33811cc153d34
Bruno Roth <tu_correo@gmail.com>
2026-07-27 23:57:36 -0300

    visitas

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import { AuthProvider } from "@/context/auth-context"
 import { ClienteProvider } from "@/context/cliente-context"
 import { StoreProvider } from "@/context/store-context"
 import { ScrollToTopOnNavigate } from "@/components/scroll-to-top-on-navigate"
-import { StatsCounter } from "@/components/stats-counter"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -49,7 +48,7 @@ export default function RootLayout({
                 <main className="min-h-screen">{children}</main>
                 <Footer />
                 <WhatsAppButton />
-                <StatsCounter />
+
               </CartProvider>
             </StoreProvider>
           </ClienteProvider>
